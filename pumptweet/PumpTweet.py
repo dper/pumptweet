e6c6e0ca2a58e26d6b8d5161d104c58fe990098b
Douglas Perkins <webmaster@dperkins.org>
2014-12-23 13:37:21 +0900

    Only run the thing if it's called directly.

--- pumptweet/PumpTweet.py
+++ pumptweet/PumpTweet.py
@@ -162,15 +162,18 @@ class PumpTweet(object):
 		self.print_tweets(tweets)
 		self.post_tweets(tweets)
 		self.update_recent()
-	
-description = 'Cross-post from pump to twitter.  Call with no options for normal use.'
-parser = argparse.ArgumentParser(description=description)
-parser.add_argument('-t', '--test', help='do a test-run without actually tweeting', action='store_true')
-args = parser.parse_args()
-pumptweet = PumpTweet()
-	
-if args.test:
-	pumptweet.pull_and_test()
-else:
-	pumptweet.pull_and_push()
 
+
+
+if __name__ == "__main__":
+	description = 'Cross-post from pump to twitter.  Call with no options for normal use.'
+	test_help = 'do a test-run without tweeting'
+	parser = argparse.ArgumentParser(description=description)
+	parser.add_argument('-t', '--test', help=test_help, action='store_true')
+	args = parser.parse_args()
+	pumptweet = PumpTweet()
+	
+	if args.test:
+		pumptweet.pull_and_test()
+	else:
+		pumptweet.pull_and_push()

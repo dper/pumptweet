7089dfec863e2ddb7b6f3b1a844f6d5977b6404b
Douglas Perkins <webmaster@dperkins.org>
2014-12-23 14:00:50 +0900

    Adding separate startup file.

--- pumptweet/PumpTweet.py
+++ pumptweet/PumpTweet.py
@@ -158,17 +158,3 @@ class PumpTweet(object):
 		self.print_tweets(tweets)
 		self.post_tweets(tweets)
 		self.update_recent()
-
-
-if __name__ == "__main__":
-	description = 'Cross-post from pump to twitter.  Call with no options for normal use.'
-	test_help = 'do a test-run without tweeting'
-	parser = argparse.ArgumentParser(description=description)
-	parser.add_argument('-t', '--test', help=test_help, action='store_true')
-	args = parser.parse_args()
-	pumptweet = PumpTweet()
-	
-	if args.test:
-		pumptweet.pull_and_test()
-	else:
-		pumptweet.pull_and_push()
--- pumptweet/pt.sh
+++ pumptweet/pt.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd `dirname $0`
 source bin/activate
-python PumpTweet.py
+pt.py
 deactivate

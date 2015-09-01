9c118ab69b58756b73b14858ff75a8ed9e6c10fa
Douglas Perkins <webmaster@dperkins.org>
2015-09-01 19:25:15 +0900

    Tweaking version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name='pumptweet',
-	version='1.5.0',
+	version='1.6.0',
 	description='Cross posts from Pump.io to Twitter.',
 	setup_requires=['setuptools-markdown'],
 	long_description_markdown_filename='README.md',

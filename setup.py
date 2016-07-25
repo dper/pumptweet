e640d9cf6852b759c884c53c566971ade756b045
Douglas Perkins <webmaster@dperkins.org>
2016-07-25 18:26:35 +0900

    Updating setup.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pumptweet',
-	version='2.0.0',
+	version='2.1',
 	description='Cross posts from Pump.io to Twitter.',
 	setup_requires=['setuptools-markdown'],
 	long_description_markdown_filename='README.md',
